e311fe5d14a94cc20167943cba71f18a1008cd2a
manujamnju-art <manujamnju@gmail.com>
2026-01-19 13:32:04 +0530

    save

---
+++ power.py
@@ -0,0 +1,8 @@
+def calculate_power(base,exponent):
+    return base*exponent
+
+if "__main__"=="__name__":
+    base = 10
+    exponent = 10
+
+    print("power : ",calculate_power())
---
+++ test_power.py
@@ -0,0 +1,4 @@
+from power import calculate_power
+
+def calculate_power():
+    assert round(calculate_power(9,8))
